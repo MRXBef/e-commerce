100cb5a8301dc66a11a47bda84c8e69ef246f42a
MRXbef <adambayu878@gmail.com>
2024-08-29 04:01:34 +0800

    menambahkan table product image dan category dan menambahkan sedikit multer middleware function

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../middleware/Multer.js";
+import upload from "../middleware/multer.js";
 import { register, getUsers, login, logout } from "../controllers/Users.js";
 import verifyToken from "../middleware/verifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
