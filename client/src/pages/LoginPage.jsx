d74f561d5e025508ac724846f59c7eb22f5397a0
MRXbef <adambayu878@gmail.com>
2024-08-30 19:45:04 +0800

    membuat halaman login di frontend + setting dikit

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import "../css/pages/LoginPage.css";
-import loginHero from '../assets/img/login-hero.png'
+import loginHero from "../assets/img/login-hero.png";
 
 const LoginPage = () => {
   return (
     <div className="login-container">
       <div className="login-box-outer">
         <div className="login-box-inner-hero">
-            <img src={loginHero} />
+          <img src={loginHero} />
         </div>
         <div className="login-box-inner-form">
-            <form>
-                <div className="login-title">
-                    <h1>Login</h1>
-                </div>
-                <input type="text" placeholder="username"/>
-                <input type="text" placeholder="password"/>
-                <button type="submit">LOGIN</button>
-            </form>
+          <form>
+            <div className="login-title">
+              <h1>Ayo, shoping :)</h1>
+            </div>
+            <div className="input-container">
+                <span className="iconku">🔒</span>
+                <input type="text" placeholder="email" />
+            </div>
+            <div className="input-container">
+                <span className="iconku">🔒</span>
+                <input type="text" placeholder="password" />
+            </div>
+            <button type="submit">LOGIN</button>
+            <div className="switch">
+              <p>
+                Belum punya akun? <a href="">Daftar</a>
+              </p>
+            </div>
+          </form>
         </div>
       </div>
     </div>
