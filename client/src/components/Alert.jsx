d6ae08eed43aa0e35ba3319b06aaf3b945c03291
MRXbef <adambayu878@gmail.com>
2024-09-15 13:42:02 +0800

    merapikan kode alert yang terpisah menjadi kode gabungan

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -31,39 +31,42 @@ export const Alert = () => {
     }, 5000);
   };
 
-  const AlertComponent = () => (
-    <div className={`alert ${alert.status ? "" : "alert-hidden"}`}>
-      <i
-        style={{
-          color: alert.isSuccess
-            ? "var(--success-color)"
-            : "var(--danger-color)",
-          position: "absolute",
-          top: "-20px",
-          padding: "5px",
-          backgroundColor: "#fff",
-          zIndex: "-1",
-          borderRadius: "50%",
-        }}
-      >
-        <CIcon icon={alert.isSuccess ? icon.cilCheckCircle : icon.cilXCircle} />
-      </i>
-      <h1
-        style={{
-          color: "#000",
-          textAlign: "center",
-          fontWeight: "500",
-        }}
-      >
-        {alert.message}
-      </h1>
-    </div>
-  );
+  const AlertComponent = () => {
+    return (
+      <div className={`alert ${alert.status ? "" : "alert-hidden"}`}>
+        <i
+          style={{
+            color: alert.isSuccess
+              ? "var(--success-color)"
+              : "var(--danger-color)",
+            top: "-20px",
+            padding: "5px",
+            backgroundColor: "#fff",
+            zIndex: "-1",
+            borderRadius: "50%",
+          }}
+        >
+          <CIcon
+            icon={alert.isSuccess ? icon.cilCheckCircle : icon.cilXCircle}
+          />
+        </i>
+        <h1
+          style={{
+            color: "#000",
+            textAlign: "center",
+            fontWeight: "500",
+          }}
+        >
+          {alert.message}
+        </h1>
+      </div>
+    );
+  };
 
   return {
     handleShowAlert,
-    AlertComponent
-  }
+    AlertComponent,
+  };
 };
 
 export const ConfirmAlert = () => {
@@ -98,43 +101,45 @@ export const ConfirmAlert = () => {
     });
   };
 
-  const ConfirmAlertComponent = () => (
-    <div
-      onClick={() =>
-        setConfirmAlert((prevState) => ({ ...prevState, show: false }))
-      }
-      className={`confirm-alert-container ${
-        confirmAlert.show ? "" : "confirm-alert-container-hidden"
-      }`}
-    >
-      <div className="confirm-alert" onClick={(e) => e.stopPropagation()}>
-        <i style={{ color: "var(--warning-color)" }}>
-          <CIcon icon={icon.cilWarning} />
-        </i>
-        <h1>{confirmAlert.msg}</h1>
-        <div>
-          <button
-            name="reject"
-            onClick={handleButtonClicked}
-            style={{ backgroundColor: "var(--danger-color)" }}
-          >
-            <i>
-              <CIcon icon={icon.cilX} />
-            </i>
-          </button>
-          <button
-            name="confirm"
-            onClick={handleButtonClicked}
-            style={{ backgroundColor: "var(--success-color)" }}
-          >
-            <i>
-              <CIcon icon={icon.cilCheck} />
-            </i>
-          </button>
+  const ConfirmAlertComponent = () => {
+    return (
+      <div
+        onClick={() =>
+          setConfirmAlert((prevState) => ({ ...prevState, show: false }))
+        }
+        className={`confirm-alert-container ${
+          confirmAlert.show ? "" : "confirm-alert-container-hidden"
+        }`}
+      >
+        <div className="confirm-alert" onClick={(e) => e.stopPropagation()}>
+          <i style={{ color: "var(--warning-color)" }}>
+            <CIcon icon={icon.cilWarning} />
+          </i>
+          <h1>{confirmAlert.msg}</h1>
+          <div>
+            <button
+              name="reject"
+              onClick={handleButtonClicked}
+              style={{ backgroundColor: "var(--danger-color)" }}
+            >
+              <i>
+                <CIcon icon={icon.cilX} />
+              </i>
+            </button>
+            <button
+              name="confirm"
+              onClick={handleButtonClicked}
+              style={{ backgroundColor: "var(--success-color)" }}
+            >
+              <i>
+                <CIcon icon={icon.cilCheck} />
+              </i>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   return {
     handleConfirmation,
